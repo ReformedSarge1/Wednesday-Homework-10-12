fae50732ff244d0d343ea70a0bb5836a6d0a098f
Carter Cook <raymanomen3014@gmail.com>
2022-10-16 11:18:57 -0700

    Homework 10-12

---
+++ 10-12.sql
@@ -0,0 +1,13 @@
+Week 5 - Wednesday Questions 
+1. List all customers who live in Texas (use 
+JOINs) 100
+2. Get all payments above $6.99 with the Customer's Full Name 17
+3. Show all customers names who have made payments over $175(use subqueries) Ray, Richard, Steven, Grant, Stephen, Garret, Matt, Micah.
+4. List all customers that live in Nepal (use the city 
+table) Jared, Meredith, Rachel.
+5. Which staff member had the most 
+transactions? Raymond
+6. How many movies of each rating are 
+there? 1000
+7.Show all customers who have made a single payment above $6.99 (Use Subqueries) 75
+8. How many free rentals did our stores give away? 100
